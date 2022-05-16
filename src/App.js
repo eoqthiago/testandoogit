c00ce2dc9613ac0044fa2df9af6f3217ec5ac6aa
thiago rodri <contatopixthiago@gmail.com>
2022-05-16 11:40:21 -0300

    a

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Alterei isso (thiago) a
-          aaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaa
         </p>
         <a
           className="App-link"

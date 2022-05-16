fd5330b9839e603686ddc54fb6729fc81b786e12
thiago rodri <contatopixthiago@gmail.com>
2022-05-16 11:33:18 -0300

    arrumei

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alterei isso (thiago)
+          Alterei isso (thiago) a
         </p>
         <a
           className="App-link"
